d4e0598b1238bf940f72a9ae8dc6b8c6f48f3c6d
wilfft <68745408+wilfft@users.noreply.github.com>
2021-02-18 23:11:21 -0300

    refact: switched devtoll to composeEnhancers

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import burguerBuilderReducer from "./components/store/reducers/burguerBuilder";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
 
 const store = createStore(
   burguerBuilderReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
